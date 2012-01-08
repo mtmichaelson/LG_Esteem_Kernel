f876e9e2d241b0113c2d0789b17858ab7d6170d0
Matt Michaelson <mtmichaelson2000@gmail.com>
2012-01-08 15:35:34 -0600

    initial import

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Sat Jan 7 01:25:55 CST 2012"
+#define LINUX_COMPILE_TIME "01:25:55"
+#define LINUX_COMPILE_BY "lg-electronics"
+#define LINUX_COMPILE_HOST "mtmichaelson-Studio-1737"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
