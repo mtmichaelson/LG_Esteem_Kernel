491f6444d3c67638658443ad05e10c7fc675d0af
Matt Michaelson <mtmichaelson2000@gmail.com>
2012-01-17 06:19:23 -0600

    Fix HDMI and adjust voltages

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Sat Jan 7 01:25:55 CST 2012"
-#define LINUX_COMPILE_TIME "01:25:55"
+#define UTS_VERSION "#3 PREEMPT Mon Jan 16 06:47:42 CST 2012"
+#define LINUX_COMPILE_TIME "06:47:42"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "mtmichaelson-Studio-1737"
 #define LINUX_COMPILE_DOMAIN "(none)"
